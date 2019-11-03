5f6274d8c7080d6cb95e696a4b83681d7724cace
ReinaldoRojas <rojas.reinaldo@hotmail.com>
2019-11-02 20:18:08 -0500

    1

---
+++ actividades/actividad5/actividad5_resources/js/ardoraConstruPalCFG.js
@@ -0,0 +1,30 @@
+//Creado con Ardora - www.webardora.net
+//bajo licencia Attribution-NonCommercial-NoDerivatives 4.0 International (CC BY-NC-ND 4.0)
+//para otros usos contacte con el autor
+var timeAct=130; timeIni=130; timeBon=0;
+var successes=0; successesMax=6; attempts=0; attemptsMax=2;
+var score=0; scoreMax=6; scoreInc=1; scoreDec=1
+var typeGame=0;
+var tiTime=true;
+var tiTimeType=1;
+var tiButtonTime=true;
+var textButtonTime="Comenzar";
+var tiSuccesses=true;
+var tiAttempts=true;
+var tiScore=true;
+var startTime;
+var colorBack="#FFFDFD"; colorButton="#91962F"; colorText="#000000"; colorSele="#FF8000";
+var goURLNext=false; goURLRepeat=false;tiAval=false;
+var scoOk=0; scoWrong=0; scoOkDo=0; scoWrongDo=0; scoMessage=""; scoPtos=10;
+var fMenssage="Verdana, Geneva, sans-serif";
+var fActi="Verdana, Geneva, sans-serif";
+var fEnun="Verdana, Geneva, sans-serif";
+var timeOnMessage=5; messageOk="bien hecho"; messageTime="ups"; messageError="para la proxima"; messageErrorG="para la proxima"; messageAttempts="casi"; isShowMessage=false;
+var urlOk=""; urlTime=""; urlError=""; urlAttempts="";
+var goURLOk="_blank"; goURLTime="_blank"; goURLAttempts="_blank"; goURLError="_blank"; 
+borderOk="#008000"; borderTime="#FF0000";borderError="#FF0000"; borderAttempts="#FF0000";
+var cp_pal=["UklFU0dPUw","UFJJTkNJUElPUw","U0VHVVJJREFE","SU5GT1JNQVRJQ0E","UFJJTkNJUElPUw","T0JKRVRJVk9T"];var cp_ima=["","","","","",""];var cp_mp3=["","","","","",""];var cp_ogg=["","","","","",""];var cp_que=["","","","","",""];var cp_num=[7,10,9,11,10,9];
+var wordsGame="YWN0aXZpZGFkNQ"; wordsStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+function giveZindex(typeElement){var valueZindex=0; capas=document.getElementsByTagName(typeElement);
+for (i=0;i<capas.length;i++){if (parseInt($(capas[i]).css("z-index"),10)>valueZindex){valueZindex=parseInt($(capas[i]).css("z-index"),10);}}return valueZindex;}
+var au="";var cp=[];var letters=[];var posAns=0;var lettersId=[];var lettersX=[];var lettersY=[];var lettersAns=[];var answers=[];var indexGame=1;var numle=5; var fillLetter="ABCDEFGHIJKLMNÑOPQRSTUVWXYZÁÉÍÓÚÜ";
